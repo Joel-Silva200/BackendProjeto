c6d42a70541f8e2eb2bb8f87c6ee194822e84b0b
Joel-Silva200 <121094543+Joel-Silva200@users.noreply.github.com>
2023-02-01 16:41:39 +0000

    Update auth.js

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -34,13 +34,14 @@ router.post("/login", async (req,res,next) => {
         if (!user) return res.status(400).json("Email incorreto!")
 
         // const pass = md5({password:req.body.password})
-        // const passFind = User.findOne({password:req.body.password})
-        const passFind = await bcrypt.compare(req.body.password, user.email)
+         const passFind = User.findOne({password:req.body.password})
+        // const passFind = await bcrypt.compare(req.body.password, user.email)
         if (!passFind) return res.status(400).json("Palavra-passe errada ou utilizador inexistente!")
 
         // const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT)
         
         // res.cookie("access_token", token, {httpOnly: true}).status(200).json(user)
+        res.status(200).json(user)
     } catch (err) {
         next(err);
     }
