6e6a482cc0610913bf0cc263d84661ec3fed152e
Joel-Silva200 <121094543+Joel-Silva200@users.noreply.github.com>
2023-02-08 23:17:52 +0000

    Update quarto.js

--- Controllers/quarto.js
+++ Controllers/quarto.js
@@ -68,6 +68,15 @@ router.delete("/:id/:idHotel", async (req,res,next) => {
     }
 });
 
+router.delete("/:id", async (req,res,next) => {
+    try {
+      await quarto.findByIdAndDelete(req.params.id);
+      res.status(200).json("Quarto apagado.");
+    } catch (err) {
+      next(err);
+    }
+});
+
 router.get("/:id", async (req,res,next) => {
     try {
       const quarto1 = await quarto.findById(req.params.id);
